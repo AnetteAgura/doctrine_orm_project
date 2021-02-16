487391fe99039cc0c1faa2e71961f6e9bf1f293e
Anette <anetteagura@gmail.com>
2021-02-16 18:25:21 +0200

    Järele tehtud, mis oli vaja.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -19,23 +19,25 @@ class AdminController extends Controller
         return $this->renderPage($response, 'admin/view.html', [
             'articles' => $articles
         ]);
-    }
+    }       
 
     public function create(Request $request, Response $response, $args = [])
     {
         $article = new Article;
+
+        
 
         if($request->isPost()){
             $article->setName($request->getParam('name'));
             $article->setSlug($request->getParam('slug'));
             $article->setImage($request->getParam('image'));
             $article->setBody($request->getParam('body'));
-            $article->setPublished(new \DateTime);
+            $article->setPublish(new \DateTime);
 
             $this->ci->get('db')->persist($article);
             $this->ci->get('db')->flush();
 
-            return $response->withRedirect('/admin');
+            return $response-> withRedirect('/admin');
         }
 
         return $this->renderPage($response, 'admin/create.html', [
@@ -57,20 +59,45 @@ class AdminController extends Controller
                 $this->ci->get('db')->remove($article);
                 $this->ci->get('db')->flush();
 
-                return $response->withRedirect('/admin');
+                return $response-> withRedirect('/admin');
             }
 
             $article->setName($request->getParam('name'));
             $article->setSlug($request->getParam('slug'));
             $article->setImage($request->getParam('image'));
             $article->setBody($request->getParam('body'));
+            
+            $article->setAuthor(
+                $this->ci->get('db')->find('App\Entity\Author', $request->getParam('author'))
+            );
 
             $this->ci->get('db')->persist($article);
             $this->ci->get('db')->flush();
         }
 
+        $authors = $this->ci->get('db')->getRepository('App\Entity\Author')->findBy([], [
+            'name' => 'ASC'
+        ]);
+
         return $this->renderPage($response, 'admin/edit.html', [
-            'article' => $article
+            'article' => $article,
+            'authors' => $this->authorDropdown($authors, $article)
         ]);
     }
+
+    private function authorDropdown($authors, $article){
+        $options =  [];
+
+        foreach ($authors as $author) {
+            $options[]=sprintf(
+                '<option value="%s" %s>%s</option>',
+                $author->getId(),
+                ($author == $article->getAuthor()) ? 'selected' : '', 
+                $author->getName()
+            );
+        }
+
+        return implode($options);
+    }
+    
 }
